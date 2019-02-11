f6ef2736bffcb19f79aa982fc7218a1a6e22a061
Edwin Joseph <edwin@wonderbly.com>
2019-02-11 23:45:55 +0000

    Serialize model and pass it to the window

--- src/server/express/configure-view-engine.ts
+++ src/server/express/configure-view-engine.ts
@@ -1,7 +1,12 @@
 import { Application } from 'express';
 import path from 'path';
+import serialize from 'serialize-javascript';
 
 export default function (app: Application): void {
   app.set('view engine', 'pug');
   app.set('views', path.join(process.cwd(), 'src/server/views'));
+
+  app.locals = {
+    serialize
+  };
 }
